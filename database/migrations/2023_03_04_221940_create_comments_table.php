b9c9fac5f9ee37eb8f461474ff836ff89f34e241
G-Wade <gareth.wade@btinternet.com>
2023-03-05 00:15:01 +0000

    coursework 1

---
+++ database/migrations/2023_03_04_221940_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('content');
+            $table->bigInteger('account_id')->unsigned();
+            $table->bigInteger('post_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('account_id')->references('id')->on('accounts')
+                ->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('post_id')->references('id')->on('posts')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
